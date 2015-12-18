8f998c5a90ad8041d5632366d6cfe9b4b0cf86b6
Cesar Ghali <cesarghali.p@gmail.com>
2015-12-17 22:22:52 -0800

    Update experiment script

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-./waf --run="pitfull-att --time=1000 --intdelay=delay-int-pitfull-att.txt --contentdelay=delay-data-pitfull-att.txt"
-./waf --run="pitfull-dfn --time=1000 --intdelay=delay-int-pitfull-dfn.txt --contentdelay=delay-data-pitfull-dfn.txt"
-./waf --run="pitless-att --time=1000 --intdelay=delay-int-pitfless-att.txt --contentdelay=delay-data-pitfless-att.txt"
-./waf --run="pitless-dfn --time=1000 --intdelay=delay-int-pitfless-dfn.txt --contentdelay=delay-data-pitfless-dfn.txt"
+mkdir data_OnInterest
+mkdir data_OnData
+
+for i in `seq 1 1`
+do
+   echo $i
+   ./waf --run="pitfull-dfn --time=1000 --intdelay=data_OnInterest/delay-int-pitfull-dfn-${i}.txt --contentdelay=data_OnData/delay-data-pitfull-dfn-${i}.txt --firstinterestdatadelay=data_RTT/delay-rtt-pitfull-dfn-${i}.txt"
+   ./waf --run="pitless-dfn --time=1000 --intdelay=data_OnInterest/delay-int-pitless-dfn-${i}.txt --contentdelay=data_OnData/delay-data-pitless-dfn-${i}.txt --firstinterestdatadelay=data_RTT/delay-rtt-pitless-dfn-${i}.txt"
+   ./waf --run="pitfull-att --time=1000 --intdelay=data_OnInterest/delay-int-pitfull-att-${i}.txt --contentdelay=data_OnData/delay-data-pitfull-att-${i}.txt --firstinterestdatadelay=data_RTT/delay-rtt-pitfull-att-${i}.txt"
+   ./waf --run="pitless-att --time=1000 --intdelay=data_OnInterest/delay-int-pitless-att-${i}.txt --contentdelay=data_OnData/delay-data-pitless-att-${i}.txt --firstinterestdatadelay=data_RTT/delay-rtt-pitless-att-${i}.txt"
+done
